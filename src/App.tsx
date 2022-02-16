a8043b687545c7e0a552e0a0d52538fb358c5370
Vaz-Tiago <tvaz@redventures.com>
2022-02-16 16:37:48 -0300

    feat: test ci/cd

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Teste deploy</p>
+        <p>Teste ci/cd</p>
         <a
           className="App-link"
           href="https://reactjs.org"
