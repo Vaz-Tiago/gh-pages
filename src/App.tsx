76a32a372dcd1e2ee6f2920e184a530b1e57cb62
Vaz-Tiago <tvaz@redventures.com>
2022-02-17 10:15:46 -0300

    feat: change home

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Teste ci/cd</p>
+        <p>TTenting the pull request action</p>
         <a
           className="App-link"
           href="https://reactjs.org"
